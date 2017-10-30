b7fe66560415afc3b328617664612dd97c6d3fa2
Matthias <git.heiming@outlook.com>
2017-10-30 13:53:12 +0100

    can create axes

--- src/common.ts
+++ src/common.ts
@@ -1,6 +1,24 @@
-export interface IVisualizationOptions {
-    height: 500,
-    width: 1000,
+export interface IVisualizationOptionsArg {
+    height?: number,
+    width?: number,
+    margin?: IMargin
+}
+
+export class IVisualizationOptions implements IVisualizationOptionsArg {
+    height: number;
+    width: number;
+    margin: IMargin;
+
+    constructor(vizOptions: IVisualizationOptionsArg = {}) {
+        this.height = vizOptions.height ? vizOptions.height : 500;
+        this.width = vizOptions.width ? vizOptions.width: 1000;
+        this.margin = vizOptions.margin ? vizOptions.margin : {
+            top: 30,
+            right: 80,
+            bottom: 20,
+            left: 80
+        }
+    }
 }
 
 export interface IDataPoint {}
@@ -9,4 +27,12 @@ export abstract class SteelCloud3DViz {
     protected data: IDataPoint[];
     public abstract getVisualization() : HTMLDivElement;
     public abstract updateData(data: IDataPoint[]): void;
+    protected visualization: HTMLDivElement;
+}
+
+export interface IMargin {
+    top: number,
+    bottom: number,
+    left: number, 
+    right: number
 }
--- src/inStock.ts
+++ src/inStock.ts
@@ -1,22 +1,65 @@
-import {SteelCloud3DViz, IDataPoint} from "./common";
+import {SteelCloud3DViz, IDataPoint, IVisualizationOptionsArg, IVisualizationOptions} from "./common";
+import * as d3 from "d3";
 
 export interface IStockDatum extends IDataPoint {
-    
+    name: string,
+    inStock: number
 }
 
 export class InStockGraph extends SteelCloud3DViz{
-    protected data: IStockDatum[]
-    constructor(stockData: IStockDatum[]) {
+    protected data: IStockDatum[];
+    protected vizOptions: IVisualizationOptions;
+    protected visualization: HTMLDivElement;
+
+    constructor(stockData: IStockDatum[], vizOptions?: IVisualizationOptionsArg) {
         super()
         this.data = stockData;
+        this.vizOptions = new IVisualizationOptions(vizOptions)
+
+        this.visualization = this.createVisualization();
     }
 
-    public getVisualization() {
-        return document.createElement("div");
+    private createVisualization() {
+        let opts = this.vizOptions;
+        let canvasWidth = opts.width - opts.margin.left - opts.margin.right;
+        let canvasHeight = opts.height - opts.margin.top - opts.margin.bottom;
+        let div = document.createElement("div");
+        let svgNode = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        let svg = d3.select(svgNode)
+            .attr("xmlns", "http://www.w3.org/2000/svg")
+            .attr("viewBox", `0 0 ${opts.width} ${opts.height}`);
+        div.appendChild(svgNode);
+        let canvas = svg.append("g")
+            .attr("transform", `translate( ${opts.margin.left}, ${opts.margin.top})`)
+        let scales = {
+            x: d3.scaleBand().range([0, canvasWidth]).padding(0.1).domain(this.data.map(d => d.name)),
+            y: d3.scaleLinear().range([canvasHeight, 0]).domain([0, Math.max(...this.data.map(d => d.inStock))])
+        }
+        let axes = {
+            x: d3.axisBottom(scales.x),
+            y: d3.axisLeft(scales.y)
+        }
+        canvas.append("g")
+            .classed("axis", true)
+            .classed("axis-x", true)
+            .attr("transform", `translate(0, ${canvasHeight})`);
+
+        canvas.append("g")
+            .classed("axis", true)
+            .classed("axis-y", true)
+
+        canvas.select(".axis-x").call(axes.x);
+        canvas.select(".axis-y").call(axes.y);                
+        
+        return div;
     }
 
     public updateData(newData: IStockDatum[]) {
         this.data = newData
+    }
+
+    public getVisualization() {
+        return this.visualization;
     }
 
     
